55eec2172d72676ee631b08088c349b93c5c94eb
Roshan R <roshan.atl@gmail.com>
2016-09-23 14:29:15 +0530

    Update ParseRequest.java

--- src/main/java/com/walmart/ocr/model/ParseRequest.java
+++ src/main/java/com/walmart/ocr/model/ParseRequest.java
@@ -4,6 +4,8 @@ public class ParseRequest {
 	private String id;
 	private String FrontText;
 	private String BackText;
+	private String FrontTextFormatted;
+	private String BackTextFormatted;
 	public String getId() {
 		return id;
 	}
@@ -22,6 +24,18 @@ public class ParseRequest {
 	}
 	public void setBackText(String backText) {
 		BackText = backText;
+	}
+	public String getFrontTextFormatted() {
+		return FrontTextFormatted;
+	}
+	public void setFrontTextFormatted(String frontTextFormatted) {
+		FrontTextFormatted = frontTextFormatted;
+	}
+	public String getBackTextFormatted() {
+		return BackTextFormatted;
+	}
+	public void setBackTextFormatted(String backTextFormatted) {
+		BackTextFormatted = backTextFormatted;
 	}
 	
 	
