fe93a6194bb882aa7eaad45bae13f135e033d281
U46591 <abdul.pkhader@ust-global.com>
2016-11-15 20:43:13 +0530

    Add files via upload

--- src/main/java/com/walmart/ocr/util/Main.java
+++ src/main/java/com/walmart/ocr/util/Main.java
@@ -3,13 +3,18 @@ package com.walmart.ocr.util;
 import java.io.File;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.walmart.ocr.dao.OcrInfoDao;
 import com.walmart.ocr.model.SmartOCRDataModel;
-
+/**
+ * 
+ * @author abkha1
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -22,20 +27,36 @@ public class Main {
 		ocrDataModel.setImageUrls("ImagesToProcess/test.jpg");
 		//ocrDataModel.setImage(ImageToByteConverter.convertImageToByte(ocrDataModel.getImageUrls(), "jpg"));
 		Serializable id = null;
-
-		id=3;//id = dao.createOcrData(ocrDataModel);
-		System.out.println("---- > " + id);
+		ocrDataModel.setStatus("waiting");
+		id = dao.createOcrData(ocrDataModel);
+		System.out.println("************************* xxxxxxxxx *************************");
+		System.out.println("****************************"+id+"***************************");
+		System.out.println("************************* xxxxxxxxx *************************");
 		ocrDataModel = dao.findOcrDataById(id);
-		System.out.println(ocrDataModel);
+		System.out.println("*  status 1 *"+ocrDataModel.getStatus());
+		System.out.println("*  status 1 *"+ocrDataModel.getAbsoobaResponse());
 
-		// ocrDataModel.setImageUrls("image is modified");
+		 ocrDataModel.setStatus("modified status");
+		 ocrDataModel.setAbsoobaResponse("modified absooba");
 		ocrDataModel.setImageUploadDate(new Date());
 		dao.updateOcrData(ocrDataModel);
 		ocrDataModel = dao.findOcrDataById(ocrDataModel.getOcrRequestId());
-		System.out.println("------------ > modified ocr data " + ocrDataModel);
-		File imageFile=ImageToByteConverter.convertByteArraytoImages(ocrDataModel.getImage(), ocrDataModel.getImageUrls(), "jpg");
+		System.out.println("*  status 2*"+ocrDataModel.getStatus());
+		System.out.println("*  status 2*"+ocrDataModel.getAbsoobaResponse());
+		//File imageFile=ImageToByteConverter.convertByteArraytoImages(ocrDataModel.getImage(), ocrDataModel.getImageUrls(), "jpg");
 		System.out.println("----- conversion completed from byte to image file");
+		System.out.println("************************* xxxxxxxxx *************************");
+		System.out.println("****************************"+id+"***************************");
+		System.out.println("************************* xxxxxxxxx *************************");
+		
+		List<SmartOCRDataModel> dataModels= dao.findOcrDataByStatus("waiting", 1, 10);
 
+		System.out.println("** "+dataModels+" **\n");
+		System.out.println("** "+dataModels.size()+" **");
+		
+		ocrDataModel = dao.findOcrDataById(17);
+		System.out.println("*  status 2*"+ocrDataModel.getStatus());
+		System.out.println("*  status 2*"+ocrDataModel.getAbsoobaResponse());
 	}
 
 }
