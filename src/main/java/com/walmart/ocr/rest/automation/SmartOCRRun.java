8e4cef2f4a2f544e7341acbdb823c37e1b28996e
Rahul <Rahul.Nair@ust-global.com>
2016-11-23 09:39:49 +0530

    Modified script for automation
    Automation script correction.

--- src/main/java/com/walmart/ocr/rest/automation/SmartOCRRun.java
+++ src/main/java/com/walmart/ocr/rest/automation/SmartOCRRun.java
@@ -42,47 +42,66 @@ public class SmartOCRRun {
 		List<File> listFiles = (List<File>) FileUtils.listFiles(file, null, false);
 		Collections.sort(listFiles);
 		Map<String, List<File>> fileGroups = new HashMap<String, List<File>>();
-		for(File imgFile : listFiles) {
-			String fName = imgFile.getName();
-			
-			String upc = fName.substring(0, fName.indexOf("."));
-			if (!fileGroups.containsKey(upc)) {
-				fileGroups.put(upc, new ArrayList<File>());
-			}			
-			String newFileName = imgFile.getAbsolutePath().replaceFirst("\\.", "-");
-			File newFile = new File(newFileName);
-			imgFile.renameTo(newFile);
-			fileGroups.get(upc).add(newFile);			
-		}
-		
 		Map<String, List<Map<String, Object>>> gtinsAttribMap = new HashMap<String, List<Map<String,Object>>>();
-		for(Entry<String, List<File>> fileEntry : fileGroups.entrySet()) {
-			List<File> files = fileEntry.getValue();
-			String upc = fileEntry.getKey();
-			List<File> filesToProcess = new ArrayList<File>();
-			boolean isMoreThanTwoFiles = files.size() > 2;
-			
-			if (isMoreThanTwoFiles) {
-				filesToProcess.add(files.get(0));
-				filesToProcess.add(files.get(2));
-			} else {
-				filesToProcess.add(files.get(0));
-				filesToProcess.add(files.get(1));
+		try {
+			for(File imgFile : listFiles) {
+				String fName = imgFile.getName();
+				
+				String upc = "";
+				if (!fName.contains("-")) {
+					upc = fName.substring(0, fName.indexOf("."));
+					if (!fileGroups.containsKey(upc)) {
+						fileGroups.put(upc, new ArrayList<File>());
+					}			
+					String newFileName = imgFile.getAbsolutePath().replaceFirst("\\.", "-");
+					File newFile = new File(newFileName);
+					imgFile.renameTo(newFile);
+					fileGroups.get(upc).add(newFile);			
+				} else {
+					upc = fName.substring(0, fName.indexOf("-"));
+					if (!fileGroups.containsKey(upc)) {
+						fileGroups.put(upc, new ArrayList<File>());
+					}
+					fileGroups.get(upc).add(imgFile);
+				}
+				
 			}
 			
-			try {
-				List<Map<String, Object>> attribForGTIN = processFiles(filesToProcess);
+			
+			for(Entry<String, List<File>> fileEntry : fileGroups.entrySet()) {
+				List<File> files = fileEntry.getValue();
+				String upc = fileEntry.getKey();
+				List<File> filesToProcess = new ArrayList<File>();
+				boolean isMoreThanTwoFiles = files.size() > 2;
 				
-				gtinsAttribMap.put(upc, attribForGTIN);
+				if (isMoreThanTwoFiles) {
+					filesToProcess.add(files.get(0));
+					filesToProcess.add(files.get(2));
+				} else {
+					filesToProcess.add(files.get(0));
+					if (files.size() > 1){
+						filesToProcess.add(files.get(1));
+					}
+				}
 				
-			} catch (Exception e) {
-				System.out.println("Parsing UPC Images caused some issue : "+ upc);
-				e.printStackTrace();
+				try {
+					List<Map<String, Object>> attribForGTIN = processFiles(filesToProcess);
+					gtinsAttribMap.put(upc, attribForGTIN);
+					
+				} catch (Exception e) {
+					System.out.println("Parsing UPC Images caused some issue : "+ upc);
+					e.printStackTrace();
+				}
+				
 			}
+			GenerateExcel.generateReport(gtinsAttribMap);
+		} catch (Exception e) {
+			GenerateExcel.generateReport(gtinsAttribMap);
+			e.printStackTrace();
 			
 		}
 		
-		GenerateExcel.generateReport(gtinsAttribMap);
+		
 		
 		return Response.ok(gtinsAttribMap).type(MediaType.APPLICATION_JSON).build();
 		
@@ -91,6 +110,8 @@ public class SmartOCRRun {
 	
 	private List<Map<String, Object>> processFiles(List<File> imageFiles) {
 		ParseRequest request = gVisionResource.parseFileRequest(imageFiles);
+		request.setFrontText(request.getFrontText().replaceFirst("Optimized by www.Imageoptimizer.net", ""));
+		request.setBackText(request.getBackText().replaceFirst("Optimized by www.Imageoptimizer.net", ""));
 		//abResource.uploadFile1(request, "guest", false);
 		List<Map<String, Object>> processParseRequestWithAbzooba = abResource.processParseRequestWithAbzooba(request);
 		
--- src/main/java/com/walmart/ocr/util/GenerateExcel.java
+++ src/main/java/com/walmart/ocr/util/GenerateExcel.java
@@ -15,49 +15,48 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 public class GenerateExcel {
-
+	
 	public static void generateReport(Map<String, List<Map<String, Object>>> maps) {
 		
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("Report");
 		
 		int rownum = 0;
-		HSSFCellStyle style = workbook.createCellStyle();
-		style.setBorderTop((short) 6); // double lines border
-		style.setBorderBottom((short) 1); // single line border
-		
-		HSSFFont font = workbook.createFont();
-		font.setFontHeightInPoints((short) 15);
-		font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-		style.setFont(font);
-		
-		Row header = sheet.createRow(rownum++);
-		header.setRowStyle(style);
-		Cell col1 = header.createCell(0);
-		
-		col1.setCellValue("GTIN");
-		Cell col2 = header.createCell(1);
-		col2.setCellValue("Attribute");
-		Cell col3 = header.createCell(2);
-		col3.setCellValue("Value");
-		Cell col4 = header.createCell(3);
-		col4.setCellValue("Confidence Level");
-		
-		
 		for(Map.Entry<String, List<Map<String, Object>>> entry : maps.entrySet()){
 			String key = entry.getKey();
 			List<Map<String, Object>> attrs = entry.getValue();
 			
+			HSSFCellStyle style = workbook.createCellStyle();
+			style.setBorderTop((short) 6); // double lines border
+			style.setBorderBottom((short) 1); // single line border
+			
+			HSSFFont font = workbook.createFont();
+			font.setFontHeightInPoints((short) 15);
+			font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+			style.setFont(font);
+			
+			int columnIndex = 0;
+			int headerIndex = 0;
+			if(rownum==0){
+				Row header = sheet.createRow(rownum++);
+				header.setRowStyle(style);
+				Cell col1 = header.createCell(headerIndex);
+				col1.setCellValue("GTIN");
+			}
+			
+			Row row = sheet.createRow(rownum++);
+			Cell gtinCell = row.createCell(columnIndex);
+			gtinCell.setCellValue(key);
 			
 			//list of attrs
 			for(Map<String, Object> attr : attrs){
-				Row row = sheet.createRow(rownum++);
-				
-				int columnIndex = 0;
-				Cell gtinCell = row.createCell(columnIndex);
-				gtinCell.setCellValue(key);
-				Cell cell1 = row.createCell(++columnIndex);
-				cell1.setCellValue(String.valueOf(attr.get("Attribute")));
+				if(rownum==2){
+					Row header = sheet.getRow(0);
+					Cell col2 = header.createCell(++headerIndex);
+					col2.setCellValue(String.valueOf(attr.get("Attribute"))+" - Value");
+					Cell col4 = header.createCell(++headerIndex);
+					col4.setCellValue(String.valueOf(attr.get("Attribute"))+" - Confidence");
+				}
 				
 				Cell cell2 = row.createCell(++columnIndex);
 				cell2.setCellValue(String.valueOf(attr.get("Value")));
@@ -69,7 +68,7 @@ public class GenerateExcel {
 		}
 		
 		try {
-			FileOutputStream out = new FileOutputStream(new File("D:\\Report.xls"));
+			FileOutputStream out = new FileOutputStream(new File("D:\\Report3.xls"));
 			workbook.write(out);
 			out.close();
 			System.out.println("Excel written successfully..");
