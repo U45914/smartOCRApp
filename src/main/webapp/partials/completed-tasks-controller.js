e3683cd65a67dd927419c97352a876a47bc3eab1
U45914 <Rahul.Nair@ust-global.com>
2016-12-05 13:43:27 +0530

    changes for completed and my tasks

---
+++ src/main/webapp/partials/completed-tasks-controller.js
@@ -0,0 +1,51 @@
+(function(){
+	
+	'use strict';
+	
+	 angular.module('smartOCR').controller('CompletedTasksController', CompletedTasksController);
+	 
+	 CompletedTasksController.$inject =['$scope','$location','$http', 'CompletedTasksServices', '$rootScope'];
+	 
+	 function CompletedTasksController($scope, $location, $http, CompletedTasksServices, $rootScope){
+		 
+		 var ctcvm = this;
+		 ctcvm.viewAllTasks = viewAllTasks;
+		 ctcvm.viewTask = viewSingleTask;
+		 ctcvm.allTasks  = null;
+		 
+		 $rootScope.$broadcast('stop-spinner');
+		 ctcvm.app = {};
+		 ctcvm.app.name = "Smart OCR";
+		 ctcvm.viewAllTasks();
+
+		 function viewSingleTask(ele) {
+			 $rootScope.$broadcast('start-spinner');
+			$('#myModal').modal('show');
+			ctcvm.selected = ctcvm.allTasks[ele];
+			 $rootScope.$broadcast('stop-spinner');
+		}
+		 
+		 function viewAllTasks(){
+			 $rootScope.$broadcast('start-spinner');
+			 CompletedTasksServices.viewAllTasks().then(function(response){
+				 ctcvm.allTasks = response.data;
+					if ( ctcvm.allTasks.message) {
+						$("#mainPallete").hide();
+						$("#alertError").fadeTo(2000, 500).slideUp(500, function(){
+						    $("#alertError").slideUp(500);
+						});
+					} else {
+						$("#mainPallete").show();
+						 ctcvm.allTasks.request.smartOcrId = ctcvm.allTasks.smartId;
+					}
+					$rootScope.$broadcast('stop-spinner');
+				});			 
+		 }
+		 
+		 $('.carousel').carousel({
+			    interval: false
+			});
+		 
+	 }
+	
+})();
---
+++ src/main/webapp/partials/my-task-new-controller.js
@@ -0,0 +1,153 @@
+(function (){
+	
+	'use strict';
+	
+	 angular.module('smartOCR')
+	 .controller('MyTaskNewController',MyTaskNewController);
+	 
+	 MyTaskNewController.$inject =['$scope','$location','$http', 'MyTaskServices', '$rootScope'];
+	 
+	 function MyTaskNewController($scope, $location, $http, MyTaskServices, $rootScope){
+		 
+		 var mtncvm=this;
+		 
+		 mtncvm.jsonString = null;
+		 mtncvm.app = {};
+		 mtncvm.app.name = "Smart OCR";	
+		 mtncvm.convertToJson = convertToJson;
+		 mtncvm.updateTexts = updateTexts;
+		 mtncvm.updateDB = updateDB;
+		 mtncvm.extractImagesFromMyTasks = extractImagesFromMyTasks;	
+		 mtncvm.convertTextAreaFriendly = convertTextAreaFriendly;
+		 mtncvm.isTablesVisible = false;
+		 
+		 $("#alertError").hide();
+		 $("#alertSuccess").hide();
+		 $("#mainPallete").hide();
+		 
+		 function convertToJson(data){
+			 mtncvm.jsonString = JSON.stringify(data);
+		 }
+		 
+		 /**
+		  * This is for showing the images and dots under the image for carousel. The class uses "active" or  ""
+		  */
+		 function extractImagesFromMyTasks(data){
+			 mtncvm.imagesArray = [];	
+			
+			 if(data.frontImage){
+				 var details = {};
+				 details.dotClass ="active";
+				 details.imageData = data.frontImage;
+				 details.classAttr = "item active";
+				 mtncvm.imagesArray.push(details);
+			 }
+			 if(data.backImage){
+				 var details = {};
+				 details.dotClass ="";
+				 details.imageData = data.backImage;
+				 details.classAttr = "item";
+				 mtncvm.imagesArray.push(details);
+			 }
+			 if(data.rightImage){
+				 var details = {};
+				 details.dotClass ="";
+				 details.imageData = data.rightImage;
+				 details.classAttr = "item";
+				 mtncvm.imagesArray.push(details);
+			 }
+			 if(data.leftImage){
+				 var details = {};
+				 details.dotClass ="";
+				 details.imageData = data.leftImage;
+				 details.classAttr = "item";
+				 mtncvm.imagesArray.push(details);
+			 }
+			 if(data.topImage){	
+				 var details = {};
+				 details.dotClass ="";
+				 details.imageData = data.topImage;
+				 details.classAttr = "item";
+				 mtncvm.imagesArray.push(details);
+			 }
+			 
+		 }
+		 
+		 /**
+		  * convertTextAreaFriendly is added to make the text area to parse html format.
+		  */		 
+		 function convertTextAreaFriendly(data){
+			 if(data.message == undefined || data.message == null || data.message == ""){
+				 angular.forEach(data.attributeBag, function(object, index){
+					 angular.forEach(object, function(value, key){
+						if(key == "Value"){
+							data.attributeBag[index].Value = convertToInnerText(value.replace(new RegExp(" br ",'g'), "<br/>"));
+						 }
+					 });				 
+				 });
+			 }
+			 return data;
+		 }
+		 
+		 
+		 function convertToInnerText(node){
+			 return angular.element(appendPTag(node))[0].innerText;
+		 }
+		 
+		 function appendPTag(text){
+			 return '<p>' + text + '</p>';
+		 }
+		 
+		 mtncvm.getMyTasks = function() {
+			$rootScope.$broadcast('start-spinner');	
+			MyTaskServices.getTasks().then(function(response) {	
+				mtncvm.myTask = mtncvm.convertTextAreaFriendly(response.data);	
+				mtncvm.responseText = mtncvm.myTask.request;
+				if (mtncvm.myTask.message) {
+					$("#mainPallete").hide();
+					$("#alertError").fadeTo(2000,500).slideUp(500,function() {
+						$("#alertError").slideUp(500);
+					});
+					$('#bn-item').addClass("attach_button").removeClass("btn-right");
+					$rootScope.$broadcast('stop-spinner');	
+					
+				} else {
+					$("#mainPallete").show();					
+					mtncvm.convertToJson(mtncvm.myTask.request);
+					mtncvm.extractImagesFromMyTasks(mtncvm.myTask);
+					mtncvm.myTask.request.smartOcrId = mtncvm.myTask.smartId;
+					mtncvm.isTablesVisible = true;
+					$rootScope.$broadcast('stop-spinner');	
+					$('#bn-item').removeClass("attach_button").addClass("btn-right");
+				}
+			});
+		  }
+
+		 function updateTexts() {
+			$rootScope.$broadcast('start-spinner');
+			MyTaskServices.updateTexts(mtncvm.jsonString).then(function(responseData) {
+				$rootScope.$broadcast('stop-spinner');
+				mtncvm.updateDB(responseData.data);
+				$("#alertSuccess").fadeTo(2000, 500).slideUp(500,function() {
+					$("#alertSuccess").slideUp(500);
+				});
+			});
+		}
+				
+		function updateDB(data){
+			$rootScope.$broadcast('start-spinner');
+			MyTaskServices.updateTexts(data).then(function(responseData) {
+				$rootScope.$broadcast('stop-spinner');
+				$("#alertSuccess").fadeTo(2000, 500).slideUp(500,function() {
+					$("#alertSuccess").slideUp(500);
+				});
+			});
+		}
+
+		$('.carousel').carousel({
+		    interval: false
+		});
+		 
+	}
+	
+})();
