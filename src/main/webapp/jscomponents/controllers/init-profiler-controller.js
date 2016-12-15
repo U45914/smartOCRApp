e2c69cf4eff9b11c99cd92b4b49b669c70938438
U45914 <Rahul.Nair@ust-global.com>
2016-12-15 17:23:46 +0530

    New file with changes - timer incorporated

--- src/main/webapp/jscomponents/controllers/init-profiler-controller.js
+++ src/main/webapp/jscomponents/controllers/init-profiler-controller.js
@@ -5,13 +5,21 @@
 	 angular.module('smartOCR')
 	 .controller('InitProfilerController',InitProfilerController);
 	 
-	 InitProfilerController.$inject =['$scope', '$rootScope','$location', 'OCRServices'];
+	 InitProfilerController.$inject =['$scope', '$rootScope','$location', 'OCRServices', '$timeout', '$interval'];
 	 
-	 function InitProfilerController($scope, $rootScope, $location, OCRServices){
+	 function InitProfilerController($scope, $rootScope, $location, OCRServices, $timeout, $interval){
 		 
 		 var ipcvm = this;
+		 ipcvm.showSubmitButton = true;
 		 ipcvm.imageArray = null;
 		 ipcvm.parseText = parseText;
+		 ipcvm.retry = retry;
+		 ipcvm.startTimer = startTimer;			
+		 ipcvm.isTimerVisible = false;
+		 ipcvm.isButtonDisbled = true;
+		 ipcvm.isRetryButtonVisible = false;
+		 ipcvm.promise = null;
+		 ipcvm.smartId = null;
 		 
 		 activate();
 		 
@@ -26,21 +34,64 @@
 				$.each(OCRServices.getFiles(), function(key, value) {
 					data.append("file", value);
 				});
+				ipcvm.showSubmitButton = false;
+				ipcvm.isRetryButtonVisible = true;
 				OCRServices.postImageData(data).then(function(response){
-					if(response.data !== undefined){
-						console.log("started");
-						setTimeout(function(){
-							console.log("Done")
+					if(response != undefined && response.data != undefined){
+						if(response.data.smartId != undefined || response.data.smartId != null){
+							ipcvm.smartId = response.data.smartId;
+							OCRServices.setOcrId(response.data.smartId);														
 							$rootScope.$broadcast('stop-spinner');
 							OCRServices.getImageData(response.data.smartId).then(function(response){
-								OCRServices.setPreviewData(response.data);
-								$location.path("/preview");
-							});
-						},10000);
-						
+								if(response != undefined && response.data != null){								
+									OCRServices.setPreviewData(response.data);
+									$location.path("/preview");
+								}else{
+									toastr.success("Request has submitted successfully");
+									ipcvm.isButtonDisabled = true;
+									ipcvm.isRetryButtonVisible = true;									
+									ipcvm.startTimer();
+								}
+							});						
+						}else{
+							toastr.warning("The submitted request is under process. Please wait for the page get redirect automatically or click Next to continue");
+						}						
+					}else{
+						toastr.error("Oops !!. Experiencing server error. Please try again later.");
 					}
 				});
 			}
+		 
+		 function retry(){
+			 OCRServices.getImageData(OCRServices.getOcrId()).then(function(response){
+				 if (response != undefined) {
+					 	ipcvm.isButtonDisabled = false;					 	
+						OCRServices.setPreviewData(response.data);
+						$location.path("/preview");
+				 } else {
+					 toastr.warning("Your request still in progress, please wait...");
+					 ipcvm.isButtonDisabled = true;
+					 ipcvm.startTimer();									 
+				 }				 	
+				});
+		 }		 
+		 
+		
+		 function startTimer(){
+			 ipcvm.counter = 10;
+			 ipcvm.isTimerVisible = true;
+			 ipcvm.onTimeout = function(){
+					ipcvm.counter--;
+					if(!ipcvm.counter){
+						$timeout.cancel(myTimeout);	
+						ipcvm.isTimerVisible = false;
+						ipcvm.isButtonDisabled = false
+					}else{
+						myTimeout = $timeout(ipcvm.onTimeout, 1000);
+					}
+				}
+				 var myTimeout = $timeout(ipcvm.onTimeout, 1000);
+		 }
 		 
 		 
 	}
